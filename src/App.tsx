3698da267b12ec6d721e7fcec84cab15ff05584f
crowwan <crowwan@naver.com>
2023-09-07 23:27:15 +0900

    feat: 예시 코드작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { useQuery } from "react-query";
+
+const POST = [
+  { id: 1, title: "Title1" },
+  { id: 2, title: "Title2" },
+];
+
 function App() {
-  return <div></div>;
+  const postQuery = useQuery({
+    queryKey: ["POST"],
+    queryFn: () => wait(1000).then(() => [...POST]),
+  });
+
+  if (postQuery.isLoading) return <div>...Loading</div>;
+  if (postQuery.isError) return <div>Error</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <div>
+        {postQuery.data?.map((data) => (
+          <div key={data.id}>{data.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 export default App;
